61558947fee1c569a613a9694150b913b752e8ae
babemonkey <799785425@qq.com>
2025-01-22 22:58:11 +0800

    表格提供跳转页面功能

--- src/views/order/OrderManage.vue
+++ src/views/order/OrderManage.vue
@@ -54,7 +54,7 @@
           v-model:page-size="pageSize"
           :total="total"
           :page-sizes="[10, 20, 50, 100]"
-          layout="total, sizes, prev, pager, next"
+          layout="total, sizes, prev, pager, next, jumper"
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
         />
